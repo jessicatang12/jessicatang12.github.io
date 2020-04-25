7ce72d870056bd18f89eb46fd63c89d6929ad376
jessicatang12 <50188602+jessicatang12@users.noreply.github.com>
2020-04-25 18:29:49 -0400

    testing async function?

--- lab_10/libraries/writeuser.js
+++ lab_10/libraries/writeuser.js
@@ -17,4 +17,17 @@ export default writeUser;
 
 //   import writeUser from "./libraries/writeuser";
 
-// 
+// code based off of async function above and lecture
+async function writeResults(names, zipcodes, interests, dbSettings) {
+  console.log(`Form data added`);
+  const db = await open(dbSettings)
+  await db.exec('CREATE TABLE IF NOT EXISTS formResults (name varchar(100), zipcode int, interests text)');
+  await db.exec(`INSERT INTO formResults VALUES ("${names}", "${zipcodes}", "${interests}")`);
+  const result = await db.each('SELECT * FROM formResults', (err) => {
+      console.log('writeResults', err);
+  });
+  console.log('Expected form result', result);
+  return result;
+}
+
+export default writeResults;
